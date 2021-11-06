f46bc43aa4bd97f187b8191a216036be814bd38c
Kenyaokun <ky2458@columbia.edu>
2021-11-06 10:06:54 -0400

    update

--- src/cipher_ky2458/cipher_ky2458.py
+++ src/cipher_ky2458/cipher_ky2458.py
@@ -1,3 +1,26 @@
+'''
+The function:
+-------------
+Each letter is replaced by a letter some fixed number of positions down the alphabet.
+
+Inputs:
+-------------
+'text': str. The word or sentence that you want to encrypt. 
+'shift': int. It means the direction and position you want to encrypt your word or sentence.
+
+Outputs:
+-------------
+The text(str) after cipher.
+
+Example:
+-------------
+>>> text = 'K'
+>>> shift = 1
+>>> m = cipher(a, b)
+>>> print(m)
+L
+'''
+
 def cipher(text, shift, encrypt=True):
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
@@ -11,16 +34,3 @@ def cipher(text, shift, encrypt=True):
             new_text += alphabet[new_index:new_index+1]
     return new_text
 
-'''
-The function: The Caesar cipher is one of the simplest and most widely known encryption techniques. In short, each letter is replaced by a letter some fixed number of positions down the alphabet. Apparently, Julius Caesar used it in his private correspondence.
-
-Inputs: 'text'(str) means the word or sentence that you want to encrypt. 'shift'(int) means the direction and position you want to encrypt your word or sentence.
-Outputs: the text(str) after cipher.
-
-Example: 
->>> text = 'K'
->>> shift = 1
->>> m = cipher(a, b)
->>> print(m)
-L
-'''
--- tests/test_cipher_ky2458.py
+++ tests/test_cipher_ky2458.py
@@ -1,1 +1,6 @@
 from cipher_ky2458 import cipher_ky2458
+def test_cipher():
+    text = 'K'
+    expected = 'L'
+    actual = cipher(text, 1)
+    assert actual == expected, "Cipher failed."
