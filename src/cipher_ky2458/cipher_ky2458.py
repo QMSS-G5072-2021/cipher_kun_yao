503955bcf0fb68742997ceca21890d42b787c4cc
Kenyaokun <ky2458@columbia.edu>
2021-11-06 12:03:43 -0400

    update

--- src/cipher_ky2458/cipher_ky2458.py
+++ src/cipher_ky2458/cipher_ky2458.py
@@ -1,28 +1,27 @@
-"""
-The function:
--------------
-Each letter is replaced by a letter some fixed number of positions down the alphabet.
-
-Inputs:
--------------
-'text': str. The word or sentence that you want to encrypt. 
-'shift': int. It means the direction and position you want to encrypt your word or sentence.
-
-Outputs:
--------------
-The text(str) after cipher.
-
-Example:
--------------
->>> import cipher_ky2458
->>> text = 'K'
->>> shift = 1
->>> m = cipher_ky2458.cipher(a, b)
->>> print(m)
-L
-"""
-
 def cipher(text, shift, encrypt=True):
+    """
+    The function:
+    -------------
+    Each letter is replaced by a letter some fixed number of positions down the alphabet.
+
+    Inputs:
+    -------------
+    'text': str. The word or sentence that you want to encrypt. 
+    'shift': int. It means the direction and position you want to encrypt your word or sentence.
+
+    Outputs:
+    -------------
+    The text(str) after cipher.
+
+    Example:
+    -------------
+    >>> import cipher_ky2458
+    >>> text = 'K'
+    >>> shift = 1
+    >>> m = cipher_ky2458.cipher(a, b)
+    >>> print(m)
+    L
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
